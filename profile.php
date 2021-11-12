d22533ab425049f62c63bd41001042a1336600d3
Ruwi Randika <92156009+Ruwii11@users.noreply.github.com>
2021-11-11 22:54:46 -0800

    edit profile.php
    add some changes to profile.php file
    edit by ruwi

--- profile.php
+++ profile.php
@@ -8,7 +8,7 @@
     <title>Profile</title>
     <?php
     session_start();
-
+    require('api/connection.php');
     if(!isset($_SESSION['uid'])){
         header("location:index.php");
     }
@@ -17,24 +17,86 @@
 <body>
     <?php
     require('navigation.php');
+    $ids = $_SESSION['uid'];
+    $sqls = "select * from profile where credential_id = ".$ids;
+    $availability = false;
+    $result = mysqli_query($conn,$sqls);
+    if(mysqli_num_rows($result) > 0){
+        $availability = true;
+        $row = mysqli_fetch_assoc($result);
+    }
+    if(isset($_GET['res'])){
+        if($_GET['res'] == "success"){
+            echo '<div class="alert alert-success" role="alert">Success!</div>';
+        }else if($_GET['res'] == "error"){
+            echo '<div class="alert alert-danger" role="alert">Add Error Occured!</div>';
+        }else if($_GET['res'] == "error"){
+            echo '<div class="alert alert-danger" role="alert">Upload Error Occured!</div>';
+        }
+    }
     ?>
     <div class="container">
-        <div class="row">
-            <div class="col-0 col-sm-0 col-md-2 col-lg-3 col-xl-3 col-xxl-5"></div>
-            <div class="col-12 col-sm-12 col-md-8 col-lg-6 col-xl-6 col-xxl-2">
-                <form action="api/auth.php" enctype="multipart/form-data" method="post">
-                    <input type="number" name="uid" value="<?php echo $_SESSION['uid']; ?>" id="" style="display:none">
-                    <input type="text" name="fname" id="fname">
-                    <input type="text" name="lname" id="lname">
-                    <input type="file" name="file_image" id="file_image">
-                    <select name="gender" id="gender">
-                        <option value="MALE">Male</option>
-                        <option value="FEMALE">Female</option>
-                    </select>
-                    <input type="submit" name="insert_profile" value="Add / Update Profile">
-                </form>
+        <div class="d-flex justify-content-center h-100">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Your Profile</h3>
+                </div>
+                <div class="card-body">
+                <?php
+                if($availability){
+                    echo '<img src="profile_pic/'.$row['profile_image'].'" width="100%" height="300"/>
+                    <form action="api/auth.php" enctype="multipart/form-data" method="post">
+                    <input type="number" name="uid" value="'.$_SESSION['uid'].'" id="" style="display:none">
+                    <div class="mb-3">
+                        <label for="fname" class="form-label">First Name</label>
+                        <input class="form-control" value="'.$row['fname'].'" type="text" name="fname" placeholder="Nimal" id="fname">
+                    </div>
+                    <div class="mb-3">
+                        <label for="lname" class="form-label">Last Name</label>
+                        <input type="text" value="'.$row['lname'].'" name="lname" class="form-control" placeholder="Siril" id="lname">
+                    </div>
+                    <div class="mb-3">
+                        <div class="form-label" id="file_image">Choose Profile Pic</div>
+                        <input type="file" name="file_image" id="file_image">
+                    </div>
+                    <div class="mb-3">
+                        <div class="form-label" id="gender">Select Gender</div>
+                        <select class="form-select" name="gender" id="gender">
+                            <option value="MALE">Male</option>
+                            <option value="FEMALE">Female</option>
+                        </select>
+                    </div>
+                    <input type="submit" class="btn btn-warning" name="update_profile" value="Update Profile">
+                </form>';
+                }
+                if(!$availability){
+                    echo '<form action="api/auth.php" enctype="multipart/form-data" method="post">
+                    <input type="number" name="uid" value="'.$_SESSION['uid'].'" id="" style="display:none">
+                    <div class="mb-3">
+                        <label for="fname" class="form-label">First Name</label>
+                        <input class="form-control" type="text" name="fname" placeholder="Nimal" id="fname">
+                    </div>
+                    <div class="mb-3">
+                        <label for="lname" class="form-label">Last Name</label>
+                        <input type="text" name="lname" class="form-control" placeholder="Siril" id="lname">
+                    </div>
+                    <div class="mb-3">
+                        <div class="form-label" id="file_image">Choose Profile Pic</div>
+                        <input type="file" name="file_image" id="file_image">
+                    </div>
+                    <div class="mb-3">
+                        <div class="form-label" id="gender">Select Gender</div>
+                        <select class="form-select" name="gender" id="gender">
+                            <option value="MALE">Male</option>
+                            <option value="FEMALE">Female</option>
+                        </select>
+                    </div>
+                    <input type="submit" class="btn btn-warning" name="insert_profile" value="Add Profile">
+                </form>';
+                }
+                ?>
+                </div>
             </div>
-            <div class="col-0 col-sm-0 col-md-2 col-lg-3 col-xl-3 col-xxl-5"></div>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
