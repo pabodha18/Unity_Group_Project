287b380e4973710095152b269792d3e681a02b8b
Pabodha18 <91983225+pabodha18@users.noreply.github.com>
2021-11-12 12:30:57 +0530

    Edit authentication.php file
    Edit authentication.php file by pabodha

--- api/auth.php
+++ api/auth.php
@@ -15,9 +15,9 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             $_SESSION["uid"] = $row["id"];
             $_SESSION["email"] = $row["email"];
 
-            header("location:../dashboard.php");
+            header("location:../dashboard.php");//check
         }else{
-            echo "Coult Not find user";
+            header("location:../index.php");//check
         }
     }else if(isset($_POST['register_sumbit'])){
         $email = $_POST['email'];
@@ -26,12 +26,12 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         if($pass1 == $pass2){
             $sql_st = "insert into credentials(email,password) values ('".$email."','".$pass1."')";
             if(mysqli_query($conn,$sql_st)){
-                header("location:../index.php");
+                header("location:../index.php");//changed
             }else{
-                echo "Some Error(s) occured";
+                header("location:../signup.php");//changed
             }
         }else{
-            echo "Password 1 not match with password 2";
+            header("location:../signup.php");//changed
         }
     }else if(isset($_POST['insert_profile'])) {
         $fname = $_POST['fname'];
@@ -46,18 +46,38 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
             $image_url = $_FILES['file_image']['name'];
             $profile_insert = "insert into profile (credential_id,fname,lname,profile_image,sex)values('".$uid."','".$fname."','".$lname."','".$image_url."','".$gender."')";
             if(mysqli_query($conn,$profile_insert)){
-                echo "Profile insertion Success";
+                header("location:../profile.php?res=success");//changed
             }else{
-                echo "Profile insertion Failed";
+                header("location:../profile.php?res=error");//changed
             }
         }else{
-            echo "Failed to upload file!";
+            header("location:../profile.php?res=error");//changed
+        }
+    }else if(isset($_POST['update_profile'])) {
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $gender = $_POST['gender'];
+        $uid = $_POST['uid'];
+
+        $target_dir = "../profile_pic/";
+        $target_file = $target_dir.basename($_FILES["file_image"]["name"]);
+
+        if(move_uploaded_file($_FILES['file_image']['tmp_name'],$target_file)){
+            $image_url = $_FILES['file_image']['name'];
+            $profile_insert = "update profile set fname = '".$fname."',lname = '".$lname."',profile_image = '".$image_url."',sex = '".$gender."' where credential_id = ".$uid;
+            if(mysqli_query($conn,$profile_insert)){
+                header("location:../profile.php?res=success");//changed
+            }else{
+                header("location:../profile.php?res=error");//changed
+            }
+        }else{
+            header("location:../profile.php?res=error");//changed
         }
     }else{
-        echo "Another Post";
+        header("location:../index.php");//changed
     }
 }else{
-    echo "Nothing";
+    header("location:../index.php");//changed
 }
 
 ?>
