c94f406b298af49884b6b20cecb6281b1da72077
Pabodha18 <91983225+pabodha18@users.noreply.github.com>
2021-11-12 12:36:42 +0530

    Edit post.php file
    Edit post.php file by pabodha

--- api/post.php
+++ api/post.php
@@ -15,20 +15,25 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
             $sql_upload_post = "insert into posts (title,caption,image_url,user) values ('".$title."','".$caption."','".$image_url."',".$uid.")";
             if(mysqli_query($conn,$sql_upload_post)){
-                echo "Insertion Completed";
+                header("location:../newpost.php?res=success");
             }else{
-                echo "Insertion Failed";
+                header("location:../newpost.php?res=failed_add");
             }
-
-            echo "File Upload Success!";
         }else{
-            echo "File Upload Failed!";
+            header("location:../newpost.php?res=failed_upload");
         }
     }else{
-        echo "incorrect";
+        header("location:../index.php");
     }
 }else{
-    echo "other";
+    if(isset($_GET['id'])){
+        $sqlsu = "delete from posts where id = ".$_GET['id'];
+        if(mysqli_query($conn,$sqlsu)){
+            header("location:../myposts.php?res=success");
+        }else{
+            header("location:../myposts.php?res=error");
+        }
+    }
 }
 
 ?>
