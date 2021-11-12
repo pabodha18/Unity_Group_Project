0e2d90379f07f6cf9e95abbb18c05aa26c18f481
Ruwi Randika <92156009+Ruwii11@users.noreply.github.com>
2021-11-11 22:58:18 -0800

    edit newpost.php
    add some changes to newpost.php file
    edit by ruwi

--- newpost.php
+++ newpost.php
@@ -18,20 +18,42 @@
 <body>
     <?php
     require('navigation.php');
+    if(isset($_GET['res'])){
+        if($_GET['res'] == "success"){
+            echo '<div class="alert alert-success" role="alert">Success!</div>';
+        }else if($_GET['res'] == "failed_add"){
+            echo '<div class="alert alert-danger" role="alert">Add Error Occured!</div>';
+        }else if($_GET['res'] == "failed_upload"){
+            echo '<div class="alert alert-danger" role="alert">Upload Error Occured!</div>';
+        }
+    }
     ?>
     <div class="container">
-        <div class="row">
-            <div class="col-0 col-sm-0 col-md-2 col-lg-3 col-xl-3 col-xxl-5"></div>
-            <div class="col-12 col-sm-12 col-md-8 col-lg-6 col-xl-6 col-xxl-2">
-                <form action="api/post.php"  enctype="multipart/form-data" method="POST">
-                    <input type="number" name="uid" style="display:none" value="<?php echo $_SESSION['uid']; ?>" id="">
-                    <input type="text" name="title" id="title">
-                    <textarea name="caption" id="caption" cols="30" rows="10"></textarea>
-                    <input type="file" name="image" id="image">
-                    <input type="submit" name="upload_new" value="Upload">
-                </form>
+        <div class="d-flex justify-content-center h-100">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Add New Image</h3>
+                </div>
+
+                <div class="card-body">
+                    <form action="api/post.php"  enctype="multipart/form-data" method="POST">
+                        <input type="number" name="uid" style="display:none" value="<?php echo $_SESSION['uid']; ?>" id="">
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Title</label>
+                            <input type="text" name="title" class="form-control" placeholder="Awesome image" id="title">
+                        </div>
+                        <div class="mb-3">
+                            <label for="caption" class="form-label">Caption</label>
+                            <textarea name="caption" id="caption" cols="30" rows="10" placeholder="Today taken at home" class="form-control"></textarea>
+                        </div>
+                        <div class="mb-3">
+                            <label for="image" class="form-label"></label>
+                            <input type="file" name="image" id="image" class="form-control">
+                        </div>
+                        <input class="btn btn-warning" type="submit" name="upload_new" value="Upload">
+                    </form>
+                </div>
             </div>
-            <div class="col-0 col-sm-0 col-md-2 col-lg-3 col-xl-3 col-xxl-5"></div>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
